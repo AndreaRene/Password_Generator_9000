c5ade03f1b4b14abda5890997a01dbe68f872125
Andrea <andrea@interhact.net>
2022-07-08 15:04:59 -0500

    working on looping function if input is invalid

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var minLength = 8;
+var maxLength = 128;
 
 // Write password to the #password input
 function writePassword() {
@@ -13,9 +15,40 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var passLength = prompt("Please choose a password length between 8 and 128 characters:")
+// generate password function will parent everything
 
-if (passLength.match(/[0-9]/)) {
-  if (passLength >= 8 && passLength <= 128) { alert("yay") } else { alert("boo") }
-} else (alert("Please enter only digits between 8 and 128."))
+function generatePassword() {
+  var passLength = getPassLength(minLength, maxLength);
+  console.log(passLength);
+  // var includeUpper = getIncludeUpper();
+  // console.log(includeUpper);
+}
 
+
+
+
+// get password length
+function getPassLength(min, max) {
+
+  var lengthInput = prompt("Please choose a password length between 8 and 128 characters:");
+
+  if (lengthInput.match(/[0-9]/) && lengthInput >= min && lengthInput <= max) {
+    return lengthInput;
+  } else {
+    alert("Your input was invalid");
+    getPassLength(minLength, maxLength);
+  }
+}
+
+// function getIncludeUpper() {
+
+//   var isUpper = prompt("Would you like to include uppercase  characters? Yes or No")
+
+//   if (isUpper.toLowerCase() === "yes" || isUpper.toLowerCase() === "y") {
+//     return true;
+//   } else if (isUpper.toLowerCase() === "no" || isUpper.toLowerCase() === "n") {
+//     return false;
+//   } else {
+//     alert("Your input was invalid");
+//   } getIncludeUpper();
+// }
