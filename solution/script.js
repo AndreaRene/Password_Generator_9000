9b8c994f508d77faa90742de9b8b57e01c713346
Andrea <andrea@interhact.net>
2022-07-08 21:43:20 -0500

    generator functioning as intended

--- solution/script.js
+++ solution/script.js
@@ -15,31 +15,42 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// generate password function will parent everything
-
+// generate password 
 function generatePassword() {
+
   var passLength = getPassLength(minLength, maxLength);
-  console.log(passLength);
-  var includeUpper = getResponse("uppercase letters?");
-  console.log(includeUpper);
-  var includeLower = getResponse("lowercase letters?");
-  console.log(includeLower);
-  var includeSpecial = getResponse("special characters?");
-  console.log(includeSpecial);
-  var includeNumber = getResponse("numbers?");
-  console.log(includeNumber);
+
+  let i = 0;
+  while (i === 0) {
+    var includeUpper = getResponse("uppercase letters?");
+    var includeLower = getResponse("lowercase letters?");
+    var includeSpecial = getResponse("special characters?");
+    var includeNumber = getResponse("numbers?");
+
+    if (includeUpper || includeLower || includeSpecial || includeNumber) {
+      i++;
+    } else {
+      alert("You must select at least one character type.")
+    }
+  }
+
+  var charList = getCharList(includeUpper, includeLower, includeSpecial, includeNumber);
+  var randomPass = "";
+
+  for (let j = 0; j < passLength; j++) {
+    randomPass += charList.charAt(Math.floor(Math.random() * charList.length));
+  }
+  return randomPass;
 }
-
-
-
 
 // get password length
 function getPassLength(min, max) {
+
   let i = 0;
   while (i === 0) {
     var lengthInput = prompt("Please choose a password length between 8 and 128 characters:");
+
     if (lengthInput.match(/[0-9]/) && !lengthInput.match(/[.]/) && lengthInput >= min && lengthInput <= max) {
-      console.log(lengthInput);
       i++;
       return lengthInput.trim();
     } else {
@@ -50,15 +61,14 @@ function getPassLength(min, max) {
 
 // get password requirements (upper, lower, special, number)
 function getResponse(condition) {
+
   let i = 0;
   while (i === 0) {
     var response = prompt("Would you like to include " + condition + " Yes or No");
     if (response.toLowerCase() === "yes" || response.toLowerCase() === "y") {
-      console.log(response);
       i++;
       return true;
     } else if (response.toLowerCase() === "no" || response.toLowerCase() === "n") {
-      console.log(response);
       i++;
       return false;
     } else {
@@ -70,4 +80,23 @@ function getResponse(condition) {
 // create character list for password generator based on user requirements
 function getCharList(includeUpper, includeLower, includeSpecial, includeNumber) {
 
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = upper.toLowerCase();
+  var number = "0123456789";
+  var special = "!#$%&'()*+,-./:;<=>?@[^_`{|}~"
+  var charSet = "";
+
+  if (includeUpper) {
+    charSet += upper;
+  }
+  if (includeLower) {
+    charSet += lower;
+  }
+  if (includeNumber) {
+    charSet += number;
+  }
+  if (includeSpecial) {
+    charSet += special;
+  }
+  return charSet;
 }
